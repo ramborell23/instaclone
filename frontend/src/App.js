4a2f52970d75551f31c67df1deccfeafda66cc6d
alejo4373 <alejo4373@gmail.com>
2018-02-15 13:25:15 -0500

    post component arrengement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import{Route, Link, Switch} from "react-router-dom";
 import logo from './instagran.jpg';
 import './App.css';
-import Users from "./users/Users";
+import Users from "./Users/Users";
 import axios from "axios";
-import LoginUser from './users/LoginUser.js';
-import NewUser from './users/NewUser.js';
+
+import LoginForm from './Users/LoginForm';
+import SignupForm from './Users/SignupForm';
+import Feed from './Users/Feed';
 
 const Footer = ({renderLogin, clickHandler}) => {
-  console.dir("what??", clickHandler)
   var footerMessage="";
   var textlink="";
   if(renderLogin === true){
@@ -30,79 +31,40 @@ const Footer = ({renderLogin, clickHandler}) => {
 
 
 class App extends Component {
-  state = { usernameInput: "", passwordInput: "", message: "", renderLogin: false };
- 
-  renderSignUpForm = () =>{
-   return (<NewUser/>);
-  }
-  renderLogInForm = () =>{
-   return (<LoginUser/>);
-  }
-
-  handleUsernameChange = e => {
-    this.setState({
-      usernameInput: e.target.value
-    });
+  state = { 
+    user: {},
+    message: "", 
+    renderLoginForm: false,
+    loggedIn: false
   };
-  
+
   toggleForms = () => {
-   const { renderLogin } = this.state
-    this.setState({renderLogin: !renderLogin})
+   const { renderLoginForm } = this.state
+    this.setState({renderLoginForm: !renderLoginForm})
+  }
+  
+  setUser = (user) => {
+    this.setState({ 
+      user: user,
+      loggedIn: true 
+    })
   }
 
-  handlePasswordChange = e => {
-    this.setState({
-      passwordInput: e.target.value
-    });
-  };
-
-  submitForm = e => {
-    e.preventDefault();
-    const { usernameInput, passwordInput } = this.state;
-
-    if (usernameInput.length < 3) {
-      this.setState({
-        message: "Username length must be at least 3"
-      });
-      return;
-    }
-
-    axios
-      .post("/users/new", {
-        username: usernameInput,
-        password: passwordInput
-      })
-      .then(res => {
-        console.log(res.data);
-        this.setState({
-          usernameInput: "",
-          passwordInput: "",
-          message: "Inserted User"
-        });
-      })
-      .catch(err => {
-        console.log("error: ", err);
-        this.setState({
-          usernameInput: "",
-          passwordInput: "",
-          message: "Error inserting user"
-        });
-      });
-    }
-
-  render() {
-    console.log('STATE ==>', this.state)
+   render() {
+    console.log('APP STATE ==>', this.state)
     console.dir(this.toggleForms)
-      const { usernameInput, passwordInput, message, renderLogin } = this.state;
+
+    const { usernameInput, passwordInput, message, renderLoginForm, loggedIn } = this.state;
+    if(loggedIn){
+      return(<Feed/>)
+    }
+
     return (
       <div className="App">
-      <div className="App-frontpage">
-           {renderLogin ? this.renderLogInForm(): this.renderSignUpForm()}
-           <Footer 
-              renderLogin={this.state.renderLogin}
-              clickHandler={this.toggleForms}
-            />
-      </div>
+        <div className="App-frontpage">
+          {renderLoginForm ? <SignupForm setUser={this.setUser}/> : <LoginForm setUser={this.setUser}/> }
+          <Footer renderLogin={this.state.renderLogin} clickHandler={this.toggleForms} />
+        </div>
       </div>
     );
   }
